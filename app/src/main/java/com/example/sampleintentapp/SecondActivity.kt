256cef80eef6827fd50bc64b284e4a76b538ec53
navarrete19970711 <mark.joseph.navarrete@gmail.com>
2024-08-28 06:42:36 +0900

    画面遷移機能
    既存ではなく、新しく作ったviewを表示させる

--- app/src/main/java/com/example/sampleintentapp/SecondActivity.kt
+++ app/src/main/java/com/example/sampleintentapp/SecondActivity.kt
@@ -1,6 +1,5 @@
 package com.example.sampleintentapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,27 +7,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // 1) viewの取得
-        val btnStart:Button = findViewById(R.id.btnstart)
+        // 3) 戻るボタン（アクティビティの終了）
+        val btnback:Button = findViewById(R.id.btnback)
 
-        // 2) ボタンを押したら次の画面へ
-        btnStart.setOnClickListener {
-            val intent = Intent(this,SecondActivity::class.java)
-            startActivity(intent)
+        btnback.setOnClickListener {
+            finish()
         }
-
-
-
     }
 }
